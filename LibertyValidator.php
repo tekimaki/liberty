efbd8e2d1b0a35d5d5ab7d9d436593913e22bd2a
Nick Palmer <nick@sluggardy.net>
2010-09-23 03:18:43 -0400

    Support new attribute validators.

--- LibertyValidator.php
+++ LibertyValidator.php
@@ -6,12 +6,14 @@
 
 class LibertyValidator {	
 
-	function preview(&$pVars, &$pParamHash, &$store) {
-		$activePlugins = LibertyValidator::getActivePlugins();
+	static $sActivePluginsByType;
+	static $sActivePluginsByAttribute;
 
+	function preview(&$pVars, &$pParamHash, &$store) {
+		LibertyValidator::setupPlugins();
 		foreach($pVars as $type => $vars) {
-			if (!empty($activePlugins[$type])) {
-				foreach ($activePlugins[$type] as $type => $data) {
+			if (!empty(LibertyValidator::$sActivePluginsByType[$type])) {
+				foreach (LibertyValidator::$sActivePluginsByType[$type] as $type => $data) {
 					if (!empty($data['preview_function'])) {
 						$function = $data['preview_function'];
 						$function($vars, $pParamHash, $store);
@@ -22,7 +24,6 @@ class LibertyValidator {
 				global $gBitSystem;
 				$gBitSystem->fatalError("Unsupported validation type: ".$type);
 			}
-			break;
 		}
 	}
 
@@ -37,47 +38,66 @@ class LibertyValidator {
 		}			
 	}
 
-	function getActivePlugins() {
+	function setupPlugins() {
 		global $gLibertySystem;
 
-		$plugins = $gLibertySystem->getPluginsOfType( VALIDATE_PLUGIN );
-		$activePlugins = array();
-		foreach ($plugins as $key => &$data) {
-			if ($data['is_active']) {
-				if (!empty($data['validate_type'])) {
-					if (is_array($data['validate_type'])) {
-						foreach($data['validate_type'] as $type) {
-							$activePlugins[$type][] = &$data;
+		if (empty(LibertyValidator::$sActivePluginsByType)) {
+			$plugins = $gLibertySystem->getPluginsOfType( VALIDATE_PLUGIN );
+			LibertyValidator::$sActivePluginsByType = array();
+			foreach ($plugins as $key => &$data) {
+				if ($data['is_active']) {
+					if (!empty($data['validate_type'])) {
+						if (is_array($data['validate_type'])) {
+							foreach($data['validate_type'] as $type) {
+								LibertyValidator::$sActivePluginsByType[$type][] = &$data;
+							}
+						} else {
+							LibertyValidator::$sActivePluginsByType[$data['validate_type']][] = &$data;
 						}
-					} else {
-						$activePlugins[$data['validate_type']][] = &$data;
+					}
+					if (!empty($data['validate_attribute'])) {
+						if (is_array($data['validate_attribute'])) {
+							foreach($data['validate_attribute'] as $attr) {
+								LibertyValidator::$sActivePluginsByAttribute[$attr][] = &$data;
+							}
+						} else {
+							LibertyValidator::$sActivePluginsByAttribute[$data['validate_attribute']][] = &$data;
+						}
 					}
 				}
 			}
+			// Stupid php scoping
+			unset($data);
 		}
-		// Stupid php scoping
-		unset($data);
-
-		return $activePlugins;
 	}
 
 	function validate(&$pVars, &$pParamHash, &$pObject, &$store) {
-		$activePlugins = LibertyValidator::getActivePlugins();
-
+		LibertyValidator::setupPlugins();
 		foreach($pVars as $type => $vars) {
-			if (!empty($activePlugins[$type])) {
-				foreach ($activePlugins[$type] as $type => $data) {
+			if (!empty(LibertyValidator::$sActivePluginsByType[$type])) {
+				foreach (LibertyValidator::$sActivePluginsByType[$type] as $plug => $data) {
 					if (!empty($data['validate_function'])) {
 						$function = $data['validate_function'];
 						$function($vars, $pParamHash, $pObject, $store);
-					}
+					}					
 				}
 			} else {
 				// TODO: Should we just call null here and ignore they turned off all validators of the right type?
 				global $gBitSystem;
 				$gBitSystem->fatalError("Unsupported validation type: ".$type);
+			}			
+			foreach($vars as $var => $constraints) {
+				foreach ($constraints as $constraint => $value) {
+					if (!empty(LibertyValidator::$sActivePluginsByAttribute[$constraint])) {
+						foreach (LibertyValidator::$sActivePluginsByAttribute[$constraint] as $plug => $data) {
+							if (!empty($data['validate_attribute_function'])) {
+								$function = $data['validate_attribute_function'];
+								$function($var, $constraints, $pParamHash, $pObject, $store);
+							}		
+						}
+					}
+				}
 			}
-			break;
 		}
 	}
 }
