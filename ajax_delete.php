b22dd409796daae5b6915a22e910f9d5f7e20499
Nick Palmer <nick@sluggardy.net>
2010-12-07 21:59:08 -0500

    Add ajax_delete.php for deleting content with an ajax.

---
+++ ajax_delete.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * required setup
+ */
+require_once( '../kernel/setup_inc.php' );
+require_once( LIBERTY_PKG_PATH."LibertyContent.php");
+
+$staticContent = new LibertyContent();
+if (isset($_REQUEST['content_id'])) {
+  $gContent = $staticContent->getLibertyObject( $_REQUEST['content_id'] );
+  if (isset($gContent) && $gContent->isValid()) {
+    if ($gContent->hasExpungePermission()) {
+      if (true || $gContent->expunge()) {
+	echo '{"status":1, "message":"'.tra("Content Deleted").'"}';
+	die;
+      } else {
+	echo '{"status":0, "message":"'.tra("Unknown Error").'"}';
+	die;
+      }
+    } else {
+      echo '{"status":0, "message":"'.tra("Permission Denied").'"}';
+      die;
+    }
+  }
+}
+echo '{"status":0, "message":"'.tra("Invalid Request").'"}';
+die;
